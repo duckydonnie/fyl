2de0eb41016654ee54af263be6f2e2ea6e4278d3
duckydonnie <sirdonnieboy@outlook.com>
2024-12-25 23:17:25 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
@@ -6,6 +7,9 @@ const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const { signOut } = useAuthenticator();
+
+  
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -32,6 +36,7 @@ function App() {
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
         </a>
+        <button onClick={signOut}>Sign out</button>
       </div>
     </main>
   );
